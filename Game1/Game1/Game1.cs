c0f9a7a23cd0c67d1485fbd20ff431b6df6f48ed
Emil Eneroth <emen02001@utb.vaxjo.se>
2021-04-19 10:38:55 +0200

    First commit

---
+++ Game1/Game1/Game1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+
+    public class Game1 : Game
+    {
+        
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        
+        Texture2D pixel;
+        Rectangle Player = new Rectangle(50, 50, 20, 20);
+        Rectangle TempLevel = new Rectangle(0, 400, 1000, 100);
+        int g = 10;
+        int OldplayerYspeed;
+        int playerYspeed = 2;
+
+        
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            pixel = Content.Load<Texture2D>("pixel");
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+            
+            Player.Y += playerYspeed;
+            if (Player.Intersects(TempLevel))
+                playerYspeed = 0;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(pixel, Player, Color.White);
+            _spriteBatch.Draw(pixel, TempLevel, Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game1/Game1/Movement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    class Movement
+    {
+        
+    }
+}
